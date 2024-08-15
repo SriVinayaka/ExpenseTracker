5693e2837b1d9b68b21b7548782ac9bf78e40371
vinaysrinivasan <vinaysrinivasan@virtusa.com>
2024-08-15 21:18:03 +0530

    React-Expense-Tracker

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import ShowData from './components/ShowData';
+import ExpenseTracker from './components/ExpenseTracker';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ShowData></ShowData>}></Route>
+          <Route path='/add' element={<ExpenseTracker></ExpenseTracker>}></Route>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
